75605977e0cee50d3d0a5e854a9845294b648fe6
blah1000 <pythonnico@outlook.de>
2024-09-28 19:50:51 +0200

    add lsp install

--- init.lua
+++ init.lua
@@ -15,25 +15,40 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Make sure to setup `mapleader` and `maplocalleader` before
+
 -- loading lazy.nvim so that mappings are correct.
 -- This is also a good place to setup other settings (vim.opt)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 require("lazy").setup({
-  spec = {{"williamboman/mason.nvim"}},
-  -- Configure any other settings here. See the documentation for more details.
+  spec = {{"williamboman/mason.nvim", 'williamboman/mason-lspconfig.nvim', 'neovim/nvim-lspconfig'}},
   -- automatically check for plugin updates
   checker = { enabled = true },
 })
 
-require("mason").setup({
-    ui = {
-        icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗"
-        }
-    }
-})
+require("mason").setup({ui = {icons = {package_installed = "✓", package_pending = "➜", package_uninstalled = "✗"}}})
+-- This function will be executed when a buffer is created
+local function on_attach(_, _)
+  -- Add your mason-lspconfig.setup() or other code here
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+  vim.keymap.set('n', '#', vim.lsp.buf.definition, {})
+  print('##')
+end
+
+-- This autocommand will execute the setup_mason_lspconfig function when a new buffer is created
+--vim.api.nvim_create_autocmd("BufNewFile,BufRead", {
+  --pattern = "*",
+  --callback = setup_mason_lspconfig,
+--})
+local mason_lspconfig = require('mason-lspconfig')
+
+-- Function to run for each installed server
+local function setup_server(server_name)
+  print("Setting up server: " .. server_name)
+  local opts = {on_attach = on_attach} -- add your server-specific options here, if any
+  require'lspconfig'[server_name].setup(opts)
+end
+
+-- Run the setup function for each server
+for _, server in ipairs(mason_lspconfig.get_installed_servers()) do setup_server(server) end
